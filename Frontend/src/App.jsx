9682ed42dc741d8adb503313b0eaabad862fb511
Suram <suramnishkala@gmail.com>
2025-09-09 09:14:44 +0530

    Save my local changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import {Header} from './layout/Header'
 import './App.css'
+import SignUppage from './pages/SignUppage';
 
 function App() {
   return (
     <>
-      <Header/>
+      <SignUppage/>
     </>
   )
 }
