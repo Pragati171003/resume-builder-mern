bd40503f6980aa7f61dc69d4d2b28af0d3a4c7e7
shirisha <puttapakashirisha02@gmail.com>
2025-09-09 10:42:37 +0530

    trying pull

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import {Header} from './layout/Header'
 import './App.css'
+import { Loginpage } from './pages/Loginpage';
 
 function App() {
   return (
     <>
       <Header/>
+      <Loginpage/>
     </>
   );
 }
