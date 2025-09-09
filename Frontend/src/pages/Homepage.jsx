5ae087657d1dbdad81724bc039948f1f8ef548a8
Pragati171003 <21ec01032@iitbbs.ac.in>
2025-09-09 08:35:23 +0530

    routing header

---
+++ Frontend/src/pages/Homepage.jsx
@@ -0,0 +1,17 @@
+
+import { Header } from '../layout/Header';
+import { Body } from '../layout/Body';
+import FAQ from '../layout/FAQ'; 
+import { Footer } from '../layout/Footer';
+
+function HomePage() {
+  return (
+    <>
+
+      <Body />
+      <FAQ />
+    </>
+  );
+}
+
+export default HomePage;
