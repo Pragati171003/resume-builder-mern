46ca298d49ca0fbea39b8d3a561b8e8facedfedf
Pragati171003 <21ec01032@iitbbs.ac.in>
2025-09-11 09:28:10 +0530

    updating login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,21 @@
 // import { Header } from './layout/Header';
 // import { Footer } from './layout/Footer';
-import { useState } from 'react';
+
 import './App.css';
 // import SignUppage from "./pages/SignUppage";
 import ResumeForm from './pages/ResumeForm.jsx';
 //import ResumeTemplate2 from './components/ResumeTemplate2.jsx'; // <-- import added
 import ResumeTemplate2 from './components/resumetemplates/ResumeTemplate2.jsx';
+import AppRoutes from './AppRoutes.jsx'
 
 function App() {
- const [submittedData, setSubmittedData] = useState(null);
 
   return (
     <>
 
       <AppRoutes />
     
-    <div>
-      {!submittedData ? (
-        <ResumeForm onSubmit={(data) => setSubmittedData(data)} />
-      ) : (
-        <ResumeTemplate2 data={submittedData}/>
-      )}
-    </div>
+    
     </>
   );
 }
