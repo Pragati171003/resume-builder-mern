8cc9664af370182501cd500ce31cf33c90454a22
Pragati171003 <21ec01032@iitbbs.ac.in>
2025-09-29 20:18:26 +0530

    Updating for live

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,21 @@ import testimonialRoutes from './routes/testimonialRoutes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173', 
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('This origin is not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
